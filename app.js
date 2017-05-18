22a9dd44e778756f37ef5e7573d8d25e833f0623
Paton Vinal <paton.vinal@gmail.com>
2017-05-18 17:20:52 -0400

    working search by name

--- app.js
+++ app.js
@@ -13,16 +13,26 @@ function addTeam(state, element){
 	state.pokeTeam.push(element);
 }
 //callback and AJAX
-const pokeApiUrl = 'http://pokeapi.co/api/v2/pokemon/';
+const pokeApiUrl = 'http://pokeapi.co/api/v2/';
 
 function getData(searchTerm) {
 const query = searchTerm;
-$.getJSON(pokeApiUrl+query+"/", function(data){
-	//console.log(data);
-	addResults(appState, data);
-	//console.log(appState);
-	renderAbility(appState, $('.results'));
-});
+
+if ($('#selectorId').val() === "name"){
+	
+	$.getJSON(pokeApiUrl+"pokemon/"+query+"/", function(data){
+		addResults(appState, data);
+		renderAbility(appState, $('.results'));
+		});
+} else {
+
+}
+// $.getJSON(pokeApiUrl+query+"/", function(data){
+// 	//console.log(data);
+// 	addResults(appState, data);
+// 	//console.log(appState);
+// 	renderPoke(appState, $('.results'));
+// });
 }
 
 //render functions
@@ -37,3 +47,10 @@ function renderAbility(state, element){
 	element.html(abilityHTML);
 }
 //event
+	$(function watchSubmit(){
+	$('.js-search-form').submit(function(event){
+		event.preventDefault();
+		const query = $('.js-query').val();
+		getData(query);
+	});
+});
