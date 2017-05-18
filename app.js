dea710eace2010246c96ded5a7a5e2e55a2cd530
Paton Vinal <paton.vinal@gmail.com>
2017-05-18 15:43:18 -0400

    adding in renderAbility

--- app.js
+++ app.js
@@ -17,11 +17,21 @@ const pokeApiUrl = 'http://pokeapi.co/api/v2/pokemon/';
 
 function getData(searchTerm) {
 const query = searchTerm;
-$.getJSON(pokeApiUrl+query, function(data){
+$.getJSON(pokeApiUrl+query+"/", function(data){
 	addResults(appState, data);
+	renderAbility(appState, $('.results'));
 });
 }
 
 //render functions
-
-//event listeners
+function renderAbility(state, element){
+	const abilityHTML = state.results.abilities.ability.map(function(obj){
+		return `
+				<ul>
+					<li>${obj}</li>
+				</ul>
+				`
+	});
+	element.html(abilityHTML);
+}
+//event 
