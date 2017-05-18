92e99ea2516f6260b54897e376a44605d5787d23
beyonce knowles <beyonce@thinkful.com>
2017-05-18 14:09:58 -0400

    working on the state

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 //state object
-
+appState = {
+	results: [],
+	pokeTeam: [],
+};
 
 //mod function
+function addResults(state, results) {
+	state.results = results;
+}
 
+function addTeam(state, element){
+	state.pokeTeam.push(element);
+}
 //callback and AJAX
+const pokeApiUrl = 'http http://pokeapi/api/v2/';
+
+function getData(searchTerm) {
+const query = {
+	q: searchTerm,
+}
+$.getJSON(pokeApiUrl, query, function(data){
+	addResults(appState, data);
+});
+}
 
 //render functions
 
